1ec8effda8c66e6925d2bec9a198e4868fd9d8d3
S1ERZANT <109344433+S1ERZANT@users.noreply.github.com>
2022-07-15 19:47:56 +0200

    Add files via upload

---
+++ COP-AC Fivem/COPPremium/__resource.lua
@@ -0,0 +1,16 @@
+description 'COP PremiumAC'
+
+
+server_scripts{
+	'@mysql-async/lib/MySQL.lua',
+	'@async/async.lua',
+	'config.lua',
+	'cs.lua',
+	'config-s.lua'
+} 
+
+client_scripts {
+	'config.lua',
+	'cc.lua',
+	'peds.lua'
+}
